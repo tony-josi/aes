b493350869bc186b78db2121dfa890deb6f98a45
Tony Josi <tonyjosinew@gmail.com>
2020-07-18 00:54:17 +0530

    basic header added __ comments added

--- inc/aes.hpp
+++ inc/aes.hpp
@@ -17,20 +17,30 @@ namespace symmetric_ciphers {
     class AES {
 
         private:
-            uint16_t key_len;           
-            uint8_t block_size;
-            uint8_t round_num;
+            uint16_t key_len;           /* Stores the length of the Key used in AES */
+            uint8_t block_size;         /* Size of the data block used */
+            uint8_t round_num;          /* Number of rounds performed */
 
         public:
-            enum key_size {
-                AES_128,
-                AES_192,
-                AES_256
+            enum key_size {             /* Enum to handle different AES Modes */
+                AES_128,                /* AES 128 bit key mode */
+                AES_192,                /* AES 192 bit key mode */
+                AES_256                 /* AES 256 bit key mode */
             };
+
+            /* Constructor */
             explicit AES(AES::key_size ks);
+
+            /* Copy Constructor */
             explicit AES(AES &aes);
+
+            /* Assignment operator for copy constructor */
             AES& operator=(const AES &aes);
+
+            /* Function to encrypt unsigned char array using AES */
             int encrpyt(const uint8_t input[], const uint8_t key[], uint8_t output[]) const;
+
+            /* Function to decrypt unsigned char array using AES */
             int decrpyt(const uint8_t input[], const uint8_t key[], uint8_t output[]) const;
             
     };
