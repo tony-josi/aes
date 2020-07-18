690bbb6e3d38d28e90020161fc4eb4f06ef89e66
Tony Josi <tonyjosinew@gmail.com>
2020-07-18 23:37:02 +0530

    function signature done for __aes_expand key

--- src/aes.cpp
+++ src/aes.cpp
@@ -11,6 +11,8 @@
 
 #include "../inc/aes.hpp"
 
+#include <stdexcept>
+
 namespace {
 
     int __aes_expand_key(
@@ -18,6 +20,30 @@ namespace {
         symmetric_ciphers::         __aes_u8   expand_key[], 
         const symmetric_ciphers::   __aes_u16  key_len
         ) {
+
+        symmetric_ciphers::__aes_u16 expand_key_len = 0;
+        symmetric_ciphers::__aes_u16 actual_key_len = 0;
+
+        switch (key_len) {
+
+        case 128:
+            actual_key_len = 16;
+            expand_key_len = 176;
+            break;
+
+        case 192:
+            actual_key_len = 24;
+            expand_key_len = 208;
+            break;
+
+        case 256:
+            actual_key_len = 32;
+            expand_key_len = 240;
+            break;
+        
+        default:
+            throw std::invalid_argument("Unsupported Key Length");
+        }
 
         return 0;
     }
