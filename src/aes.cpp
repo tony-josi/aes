27619f44aa49859b78710f745b4bc843900d1ffb
Tony Josi <tonyjosinew@gmail.com>
2020-07-21 00:47:25 +0530

    __aes_shift_rows added

--- src/aes.cpp
+++ src/aes.cpp
@@ -336,6 +336,23 @@ namespace {
                 cur_state[i][j] = AES_S_BOX[ cur_state[i][j] ];
     }
 
+    void __aes_shift_rows(
+        symmetric_ciphers::         __aes_u8    cur_state[AES_WORD_SIZE][AES_WORD_SIZE]
+    ) {
+
+        symmetric_ciphers::__aes_u8 cur_row[AES_WORD_SIZE];
+        for(int i = 0; i < AES_WORD_SIZE; ++i) {
+
+            if(i > 0) {    
+                for(int j = 0; j < AES_WORD_SIZE; ++j) 
+                    cur_row[j] = cur_state[i][j];
+                
+                for(int j = 0; j < AES_WORD_SIZE; ++j)
+                    cur_state[i][j] = cur_row[ ((i + j) % AES_WORD_SIZE) ];
+            }
+        }
+    }
+
 } /* End of anonymous namespace */
 
 
