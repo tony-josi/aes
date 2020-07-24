b0d5845e4d1d9f50cd5361f7f93961d5f6316280
Tony Josi <tonyjosinew@gmail.com>
2020-07-24 23:24:36 +0530

    test done in 3 bit modes

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@ int main() {
     my_aes.encrpyt(my_ip, my_key, my_op);
 
     for(int i = 0; i < 16; ++i)
-        std::printf("%0x", my_op[i]);
+        std::printf("%02X", my_op[i]);
     std::cout << std::endl;
 
     my_aes.decrpyt(my_op, my_key, my_plain);
@@ -24,12 +24,33 @@ int main() {
     std::cout << std::endl;
 
     /* Test for constructor with 192 bits */
+    symmetric_ciphers::__aes_u8 my_ip2[] = "&&CHRIST_ALONE&&"; 
     symmetric_ciphers::AES my_aes_copy2(symmetric_ciphers::key_size::AES_192);
     symmetric_ciphers::__aes_u8 my_key2[33] = "HELLO_THIS_XS_651234567";
-    my_aes_copy2.encrpyt(my_ip, my_key2, my_op);
+    my_aes_copy2.encrpyt(my_ip2, my_key2, my_op);
 
     for(int i = 0; i < 16; ++i)
-        std::printf("%0x", my_op[i]);
+        std::printf("%02X", my_op[i]);
+    std::cout << std::endl;
+
+    my_aes_copy2.decrpyt(my_op, my_key2, my_plain);
+    for(int i = 0; i < 16; ++i)
+        std::printf("%c", my_plain[i]);
+    std::cout << std::endl;
+
+    /* Test for constructor with 256 bits */
+    symmetric_ciphers::__aes_u8 my_ip3[] = "||CHRIST_ALONE||"; 
+    symmetric_ciphers::AES my_aes_copy3(symmetric_ciphers::key_size::AES_256);
+    symmetric_ciphers::__aes_u8 my_key3[33] = "12345678123456781234567812345678";
+    my_aes_copy3.encrpyt(my_ip3, my_key3, my_op);
+
+    for(int i = 0; i < 16; ++i)
+        std::printf("%02X", my_op[i]);
+    std::cout << std::endl;
+
+    my_aes_copy3.decrpyt(my_op, my_key3, my_plain);
+    for(int i = 0; i < 16; ++i)
+        std::printf("%c", my_plain[i]);
     std::cout << std::endl;
 
 #if 0
