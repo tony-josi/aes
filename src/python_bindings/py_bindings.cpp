5c282d4790c8b6e6381d85275d97e42185a330c7
Tony Josi <tonyjosinew@gmail.com>
2021-09-05 02:14:02 +0530

    added docstrings

--- src/python_bindings/py_bindings.cpp
+++ src/python_bindings/py_bindings.cpp
@@ -15,14 +15,16 @@ PYBIND11_MODULE(py_sc_aes, module) {
 		"128 / 192 / 256 bit keys and option for multi threading that spawns upto std::thread::hardware_concurrency() threads.";
 
 	pybind11::enum_<symmetric_ciphers::key_size>(module, "key_size")
-		.value("AES_128", symmetric_ciphers::AES_128)
-		.value("AES_192", symmetric_ciphers::AES_192)
-		.value("AES_256", symmetric_ciphers::AES_256)
+		.value("AES_128", symmetric_ciphers::AES_128, "AES 128 bit key mode")
+		.value("AES_192", symmetric_ciphers::AES_192, "AES 192 bit key mode")
+		.value("AES_256", symmetric_ciphers::AES_256, "AES 256 bit key mode")
 		.export_values();
 
 	pybind11::class_<symmetric_ciphers::AES>(module, "AES")
 		.def(pybind11::init<symmetric_ciphers::key_size>())
-		.def("encrypt_file", &symmetric_ciphers::AES::encrpyt_file__pybind_target)
-		.def("decrypt_file", &symmetric_ciphers::AES::decrpyt_file__pybind_target);
+		.def("encrypt_file", &symmetric_ciphers::AES::encrpyt_file__pybind_target, "Encrypt the given file (ip_file_name), name the output file (op_file_name), with the given key.", \
+			pybind11::arg("ip_file_name"), pybind11::arg("op_file_name") = "op_file.enc", pybind11::arg("key") = "passwd1234")
+		.def("decrypt_file", &symmetric_ciphers::AES::decrpyt_file__pybind_target, "Decrypt the given file (ip_file_name), name the output file (op_file_name), with the given key.", \
+			pybind11::arg("ip_file_name"), pybind11::arg("op_file_name") = "decrypted_noname_file", pybind11::arg("key") = "passwd1234");
 
 }
