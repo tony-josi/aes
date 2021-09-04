93b6e7a1e226692dfebc76e4c0c7e4a914b5df09
Tony Josi <tonyjosinew@gmail.com>
2021-09-05 02:45:26 +0530

    test file to check the pthon binding generated

---
+++ test/python_bindings_module_test_files/bindings_test.py
@@ -0,0 +1,16 @@
+import py_sc_aes
+import sys, time
+
+if __name__ == "__main__":
+
+    if len(sys.argv) >= 4:
+
+        prev_time = time.time()
+        aes_128_obj = py_sc_aes.AES(py_sc_aes.AES_128)
+        if sys.argv[1] == "e":
+            print(aes_128_obj.encrypt_file(sys.argv[2], sys.argv[3], sys.argv[4]))
+        elif sys.argv[1] == "d":
+            print(aes_128_obj.decrypt_file(sys.argv[2], sys.argv[3], sys.argv[4]))
+        print("Duration: ", (time.time() - prev_time) * 1000)
+        
+    
