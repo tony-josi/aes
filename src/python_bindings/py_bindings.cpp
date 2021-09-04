0c05e3ab31152635aa9899dc157ad33db2a8c874
Tony Josi <tonyjosinew@gmail.com>
2021-09-05 02:00:38 +0530

    Added basic bindings

--- src/python_bindings/py_bindings.cpp
+++ src/python_bindings/py_bindings.cpp
@@ -2,17 +2,17 @@
 
 #include "aes.hpp"
 
-PYBIND11_MODULE(py_sc_aes, module) { 
+PYBIND11_MODULE(py_sc_aes, module) {
 
 	module.doc() = "Python binding of the AES symmetric cipher algorithm library written in C++. "
-					"Uses electronic codebook mode with support for 128/192/256 bit keys. "
-					"\n"
-					"The Advanced Encryption Standard (AES), also known by its original name Rijndael "
-					"is a specification for the encryption of electronic data established by the U.S. "
-					"National Institute of Standards and Technology(NIST). "
-					"\n"
-					"This implementation currently supports Electronic codebook mode with support for "
-					"128 / 192 / 256 bit keys and option for multi threading that spawns upto std::thread::hardware_concurrency() threads.";
+		"Uses electronic codebook mode with support for 128/192/256 bit keys. "
+		"\n"
+		"The Advanced Encryption Standard (AES), also known by its original name Rijndael "
+		"is a specification for the encryption of electronic data established by the U.S. "
+		"National Institute of Standards and Technology(NIST). "
+		"\n"
+		"This implementation currently supports Electronic codebook mode with support for "
+		"128 / 192 / 256 bit keys and option for multi threading that spawns upto std::thread::hardware_concurrency() threads.";
 
 	pybind11::enum_<symmetric_ciphers::key_size>(module, "key_size")
 		.value("AES_128", symmetric_ciphers::AES_128)
