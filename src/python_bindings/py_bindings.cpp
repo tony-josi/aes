8f87d9a63b31536605485e8645698a0f4f5f518c
Tony Josi <tonyjosinew@gmail.com>
2021-09-05 02:14:53 +0530

    added docstrings

--- src/python_bindings/py_bindings.cpp
+++ src/python_bindings/py_bindings.cpp
@@ -22,9 +22,11 @@ PYBIND11_MODULE(py_sc_aes, module) {
 
 	pybind11::class_<symmetric_ciphers::AES>(module, "AES")
 		.def(pybind11::init<symmetric_ciphers::key_size>())
-		.def("encrypt_file", &symmetric_ciphers::AES::encrpyt_file__pybind_target, "Encrypt the given file (ip_file_name), name the output file (op_file_name), with the given key.", \
+		.def("encrypt_file", &symmetric_ciphers::AES::encrpyt_file__pybind_target, \
+            "Encrypt the given file (ip_file_name), name the output file (op_file_name), with the given key.", \
 			pybind11::arg("ip_file_name"), pybind11::arg("op_file_name") = "op_file.enc", pybind11::arg("key") = "passwd1234")
-		.def("decrypt_file", &symmetric_ciphers::AES::decrpyt_file__pybind_target, "Decrypt the given file (ip_file_name), name the output file (op_file_name), with the given key.", \
+		.def("decrypt_file", &symmetric_ciphers::AES::decrpyt_file__pybind_target, \
+            "Decrypt the given file (ip_file_name), name the output file (op_file_name), with the given key.", \
 			pybind11::arg("ip_file_name"), pybind11::arg("op_file_name") = "decrypted_noname_file", pybind11::arg("key") = "passwd1234");
 
 }
